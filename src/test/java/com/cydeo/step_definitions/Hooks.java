6c30376185a7ca69527a97db0d15085370598731
aysesoyturk <sytrkayse@gmail.com>
2023-09-29 12:27:20 +0300

    hooks

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,5 +1,19 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import org.junit.After;
+import org.junit.Before;
+
 public class Hooks {
+    @Before
+    public void setup(){
+        Driver.getDriver().get(ConfigurationReader.getProperty("environment"));
+
+    }
+    @After
+    public void teardown(){
+        Driver.closeDriver();
+    }
 
 }

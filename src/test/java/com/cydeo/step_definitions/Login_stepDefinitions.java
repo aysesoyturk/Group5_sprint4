e2af89099869b945bdce6109f0fafd86a6bd876f
Banu <banu2106@gmail.com>
2023-09-29 13:28:58 +0300

    Login_PositiveScenario

--- src/test/java/com/cydeo/step_definitions/Login_stepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_stepDefinitions.java
@@ -1,4 +1,52 @@
 package com.cydeo.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
 public class Login_stepDefinitions  {
+    @Given("Users go to the login page")
+    public void users_go_to_the_login_page() {
+
+    }
+
+    @When("Users see login page")
+    public void users_see_login_page() {
+
+    }
+
+    @When("Users write {string} and {string}")
+    public void users_write_and(String string, String string2) {
+
+    }
+
+    @When("Users see the Log In button")
+    public void users_see_the_log_in_button() {
+
+    }
+
+    @When("Users click to Log In button")
+    public void users_click_to_log_in_button() {
+
+    }
+
+    @When("Users see Activity Stream")
+    public void users_see_activity_stream() {
+
+    }
+
+    @When("Users go to profile")
+    public void users_go_to_profile() {
+
+    }
+
+    @When("Users click the profile")
+    public void users_click_the_profile() {
+
+    }
+
+    @When("Users see the Log Out")
+    public void users_see_the_log_out() {
+
+    }
+
 }

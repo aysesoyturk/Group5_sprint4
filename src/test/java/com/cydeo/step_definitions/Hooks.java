fab74cfa2a06c2d62b08b5e537bfb1bd4dd0b594
Banu <banu2106@gmail.com>
2023-09-29 12:26:20 +0300

    Hooks added

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,5 +1,19 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
 public class Hooks {
+    @Before
+    public void setup(){
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+
+    }
+    @After
+    public void teardown(){
+        Driver.closeDriver();
+    }
 
 }
